1dfacd16c2ff0b00d837ca6f72e397547611088c
farahderbell <150382054+farahderbell@users.noreply.github.com>
2025-11-11 14:20:01 +0100

    Add initial Airport Management core and UI projects
    Created AM.ApplicationCore and AM.UI.Console projects targeting .NET 8.0. Added domain models for Flight, Passenger, Plane, Staff, and Traveller with basic properties and relationships. Included initial project files, solution file, and build outputs.

---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PassportNumber { get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
+
+
+        public override string ToString()
+        {
+                       return $"Passenger ID: {Id}, Name: {FirstName} {LastName}, Email: {EmailAddress}, Phone: {PhoneNumber}, BirthDate: {BirthDate.ToShortDateString()}, PassportNumber: {PassportNumber}";
+        }
+
+       /* public bool CheckProfile(string firstName, string lastName)
+        {
+            return FirstName == firstName && LastName == lastName;
+        }
+        public bool CheckProfile(string firstName, string lastName, string email)
+        {
+            return FirstName == firstName && LastName == lastName && EmailAddress == email;
+        }   */
+       public bool CheckProfile(string firstName, string lastName, string? email = null)
+       {
+           if (email == null)
+           {
+               return FirstName == firstName && LastName == lastName;
+           }
+           else
+           {
+               return FirstName == firstName && LastName == lastName && EmailAddress == email;
+           }
+        }   
+        public virtual void PassengerType()
+        {
+            Console.WriteLine("I am a passenger");
+        }
+
+
+    }
+  
+}
---
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Plane
+    {
+       
+        public enum PlaneType
+        {
+            Boing,
+            Airbus,
+           
+        }   
+
+        public int Capacity { get; set; }
+        public int PlaneID { get; set; }
+        public PlaneType Type { get; set; } 
+        public DateTime ManufactureDate { get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
+        public Plane MyPlane { get; set; }
+
+        public override string ToString()
+        {
+            return $"PlaneID: {PlaneID}, Type: {Type}, Capacity: {Capacity}, ManufactureDate: {ManufactureDate.ToShortDateString()}";
+        }
+        public Plane(int planeId,int capacity, DateTime ManufactureDate, PlaneType type)
+        {
+            this.PlaneID = planeId;
+            this.Capacity = capacity;
+            this.ManufactureDate = ManufactureDate;
+            this.Type = type;
+
+
+        }
+        
+        }
+
+
+
+    }
+
